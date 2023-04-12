0fe26dcd8595e99e266d8fcb14d08df0e8397bfc
Victor 'Macrev <victorcruzrodrigues2003@gmail.com>
2023-04-12 17:48:36 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,34 +1,39 @@
 import discord
+import openai_secret_manager
 import openai
-import os
 
-# Set up the Discord client and ChatGPT API key
-client = discord.Client()
-openai.api_key = os.getenv("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")
+# Set up OpenAI API credentials
+secrets = openai_secret_manager.get_secret("openai")
+openai.api_key = secrets["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"]
 
-# Define the function that sends messages using the ChatGPT API
-async def generate_message(content):
-    prompt = f"Conversation with a Discord user:\nUser: {content}\nAI:"
-    response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=100)
-    return response["choices"][0]["text"]
+# Define the prompt to start the conversation
+prompt = "Hello, how are you doing today?"
 
-# Define the function that runs when the bot is ready
-@client.event
-async def on_ready():
-    print("Logged in as", client.user.name)
+# Define the completion parameters
+completion_parameters = {
+    "prompt": prompt,
+    "temperature": 0.7,
+    "max_tokens": 50,
+    "n_top": 1,
+    "stop": "\n"
+}
 
-# Define the function that runs when a message is received
-@client.event
-async def on_message(message):
-    # Ignore messages from the bot itself
-    if message.author == client.user:
-        return
-    
-    # Generate a response using the ChatGPT API
-    response = await generate_message(message.content)
-    
-    # Send the response back to the Discord channel
-    await message.channel.send(response)
+# Define a function to send a message to the chatbot and receive its response
+def send_message(message):
+    # Add the user's message to the prompt
+    prompt_with_message = f"{prompt}\nUser: {message}\nBot:"
+
+    # Use the OpenAI API to generate the bot's response
+    response = openai.Completion.create(engine="davinci", **completion_parameters, prompt=prompt_with_message)
+
+    # Extract the bot's response from the API response and return it
+    bot_response = response.choices[0].text.strip()
+    return bot_response
+
+# Test the chatbot by sending it a message and printing its response
+message = input("Say something to the chatbot: ")
+response = send_message(message)
+print(response)
 
 # Run the bot using your Discord bot token
 client.run("MTA5NTgwOTQ0Mjk0OTM3MDAwNg.Gg6Pr7.U72226WxS_1_b-qC_6YtJ1Uv0fP-JsUf0p_mKE")
